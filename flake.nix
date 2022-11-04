65601ee8f7ac0717b5c24986eb1c37e838ed2267
euonymos <ilia@mlabs.city>
2022-11-04 13:22:14 -0600

    version which works in pure environment

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "NixOS module for Kupo";
   nixConfig = {
-    extra-experimental-features = [ "nix-command" "flakes" "ca-derivations"];
+    extra-experimental-features = [ "nix-command" "flakes"];
     allow-import-from-derivation = "true";
     cores = "1";
     max-jobs = "auto";
     auto-optimise-store = "true";
   };
   inputs = {
-    haskell-nix.url = "github:input-output-hk/haskell.nix";
-    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    haskell-nix.url = "github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9";
+    iohk-nix.url = "github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366";
     nixpkgs.follows = "haskell-nix/nixpkgs";
     iohk-nix.inputs.nixpkgs.follows ="haskell-nix/nixpkgs";
     kupo = {
@@ -31,12 +31,14 @@
         sha256 = iohkNixPin.narHash;
       };
       system = "x86_64-linux";
-      # pkgs = import nixpkgs { inherit system; };
+      # we need it because of the way haskell.nix loads pkgs
+      pkgs = import nixpkgs { localSystem = { inherit system; }; };
       flake = ((import inputs.kupo)
         {
           inherit system;
-          haskellNix = import haskellNixSrc { }; # inherit pkgs; };
+          haskellNix = import haskellNixSrc { inherit pkgs; };
           iohkNix = import iohkNixSrc { inherit system; };
+          nixpkgsArgs = { inherit system; };
         }).flake {};
     in {
       packages.${system}.kupo = flake.packages."kupo:exe:kupo";
@@ -47,7 +49,3 @@
       };
     };
 }
-
-
-
-#

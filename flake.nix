19c053144a13b889edd64916548bc95f646d957f
Jonathan Shen <document_done@hotmail.com>
2023-02-15 20:51:01 -0500

    update kupo version. needed to add CHaP input and mapping to get it to work and update haskell.nix which for some reason the newest version worked but not tag 0.117 which kupo uses. maybe should use that instead

--- flake.nix
+++ flake.nix
@@ -9,8 +9,12 @@
   };
   inputs = {
     # should follow inputs in https://github.com/CardanoSolutions/kupo/blob/master/default.nix#L22
-    haskell-nix.url = github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9;
+    haskell-nix.url = github:input-output-hk/haskell.nix;
     iohk-nix.url = github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366;
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages/316e0a626fed1a928e659c7fc2577c7773770f7f";
+      flake = false;
+    };
 
     nixpkgs.follows = "haskell-nix/nixpkgs";
     iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
@@ -19,7 +23,7 @@
       flake = false;
     };
   };
-  outputs = inputs@{ self, nixpkgs, haskell-nix, ... }:
+  outputs = inputs@{ self, nixpkgs, haskell-nix, CHaP, ... }:
     let
       perSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       pkgs = perSystem (system: import nixpkgs { inherit system; overlays = [ haskell-nix.overlay ]; inherit (haskell-nix) config; });
@@ -34,6 +38,7 @@
               (baseNameOf path != "package.yaml")
             ];
         };
+        inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP; };
       });
       flake = perSystem (system: project.${system}.flake { });
     in

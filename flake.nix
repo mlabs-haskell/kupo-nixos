078ea6aaa242df37543e063b664040d1808fe31e
Jonathan Shen <document_done@hotmail.com>
2023-02-17 11:33:43 -0500

    update kupo. update haskell.nix to what kupo wants. add CHaP input to what kupo wants

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
   description = "NixOS module for Kupo";
-  nixConfig = {
-    extra-experimental-features = [ "nix-command" "flakes" ];
-    allow-import-from-derivation = "true";
-    cores = "1";
-    max-jobs = "auto";
-    auto-optimise-store = "true";
-  };
+  # nixConfig = {
+  #   extra-experimental-features = [ "nix-command" "flakes" ];
+  #   allow-import-from-derivation = "true";
+  #   cores = "1";
+  #   max-jobs = "auto";
+  #   auto-optimise-store = "true";
+  # };
   inputs = {
     # should follow inputs in https://github.com/CardanoSolutions/kupo/blob/master/default.nix#L22
-    haskell-nix.url = github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9;
+    # haskell-nix.url = github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9;
+    haskell-nix.url = github:input-output-hk/haskell.nix/b90fbaa272a6d17ddc21164ca3056e1618feafcd;
     iohk-nix.url = github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366;
 
     nixpkgs.follows = "haskell-nix/nixpkgs";
@@ -18,8 +19,12 @@
       url = github:CardanoSolutions/kupo;
       flake = false;
     };
+    chap = {
+      url = "github:input-output-hk/cardano-haskell-packages/316e0a626fed1a928e659c7fc2577c7773770f7f";
+      flake = false;
+    };
   };
-  outputs = inputs@{ self, nixpkgs, haskell-nix, ... }:
+  outputs = inputs@{ self, nixpkgs, haskell-nix, chap, ... }:
     let
       perSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       pkgs = perSystem (system: import nixpkgs { inherit system; overlays = [ haskell-nix.overlay ]; inherit (haskell-nix) config; });
@@ -34,6 +39,7 @@
               (baseNameOf path != "package.yaml")
             ];
         };
+        inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = chap; };
       });
       flake = perSystem (system: project.${system}.flake { });
     in

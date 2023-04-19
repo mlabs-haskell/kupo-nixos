245e967d4dbca2c706393c48dcf35c5858337cfd
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-19 17:11:30 +0200

    Updated to `v.2.4.0`
    - now it uses CHaP.
    - removed `nixConfig`.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,29 @@
 {
   description = "NixOS module for Kupo";
-  nixConfig = {
-    extra-experimental-features = [ "nix-command" "flakes" ];
-    allow-import-from-derivation = "true";
-    cores = "1";
-    max-jobs = "auto";
-    auto-optimise-store = "true";
-  };
   inputs = {
-    # should follow inputs in https://github.com/CardanoSolutions/kupo/blob/master/default.nix#L22
-    haskell-nix.url = github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9;
-    iohk-nix.url = github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366;
+    # should follow inputs in https://github.com/CardanoSolutions/kupo/blob/master/default.nix
+    haskell-nix.url = "github:input-output-hk/haskell.nix/0.0.117";
+    iohk-nix.url = "github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366";
+    cardanoPkgs = {
+      url = "github:input-output-hk/cardano-haskell-packages/316e0a626fed1a928e659c7fc2577c7773770f7f";
+      flake = false;
+    };
 
     nixpkgs.follows = "haskell-nix/nixpkgs";
     iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
     kupo = {
-      url = github:CardanoSolutions/kupo;
+      url = "github:CardanoSolutions/kupo/v2.4.0";
       flake = false;
     };
   };
-  outputs = inputs@{ self, nixpkgs, haskell-nix, ... }:
+  outputs = inputs@{ self, nixpkgs, haskell-nix, cardanoPkgs, ... }:
     let
       perSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       pkgs = perSystem (system: import nixpkgs { inherit system; overlays = [ haskell-nix.overlay ]; inherit (haskell-nix) config; });
       project = perSystem (system: pkgs.${system}.haskell-nix.project {
         compiler-nix-name = "ghc8107";
         projectFileName = "cabal.project";
+        inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = cardanoPkgs; };
         src = nixpkgs.lib.cleanSourceWith {
           name = "kupo-src";
           src = inputs.kupo;
